6bc75a61c29bb54d7dea327fcf533f6a1b0f6149
lazare-mzr <iarajulilazare@gmail.com>
2024-11-16 13:22:19 +0400

    Add files via upload

--- lazare.py
+++ lazare.py
@@ -1,1 +1,27 @@
-print ("lazare")
+for i in range (10,125):
+    if i % 5 == 0:
+        print (i)
+
+
+
+for i in range (1500,2100):
+    if i % 5 and 7 == 0:
+        print (i)
+
+
+def pirveli():
+    for i in range (1500,2100):
+        if i % 5 and 7 == 0:
+            jami = sum(pirveli)        
+print(pirveli)     
+
+
+
+
+
+
+
+
+
+
+
